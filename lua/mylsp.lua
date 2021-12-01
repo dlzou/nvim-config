e05770a2d3abd2e744f1ce1270cb83b42c39cc07
Daniel Zou <zoudl2000@gmail.com>
2021-11-30 23:04:22 -0800

    Improve diagnostic behavior

--- lua/mylsp.lua
+++ lua/mylsp.lua
@@ -4,13 +4,14 @@ function M.config()
   local lspconfig = require('lspconfig')
   local handlers = {
     ['textDocument/publishDiagnostics'] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {virtual_text = false}
+      vim.lsp.diagnostic.on_publish_diagnostics,
+      {severity_sort = true, virtual_text = false}
     ),
   }
 
   local on_attach = function(client)
     require('illuminate').on_attach(client)
-    vim.cmd [[autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics({show_header=false,focusable=false,close_events={'CursorMoved','InsertEnter'}})]]
+    vim.cmd [[autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics({show_header=false,focusable=false,close_events={'CursorMoved','InsertEnter','WinScrolled'}})]]
   end
 
   -- Pyright
